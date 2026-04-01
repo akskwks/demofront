ea8ff8f886319305de83eb64b6acf881ee14136a
akskwks <a95121587@gmail.com>
2026-04-01 15:54:10 +0900

    api 주소 수정

--- src/api/BoardApi.js
+++ src/api/BoardApi.js
@@ -1,8 +1,8 @@
-const BASE_URL = "http://localhost:8080/api/boards";
+const BASE_URL = "http://34.9.116.242/:8080/api/boards";
 
 // 게시판 목록
 export const getBoards = async (page = 0, keyword = "") => {
-  let url = `http://localhost:8080/api/boards?page=${page}&size=10`;
+  let url = `http://34.9.116.242:8080/api/boards?page=${page}&size=10`;
 
   if (keyword) {
     url += `&keyword=${keyword}`;
@@ -27,38 +27,43 @@ export const createBoard = async (data) => {
 
 // 게시글 상세 보기
 export const getBoardDetail = async (boardId) => {
-  const response = await fetch(`http://localhost:8080/api/boards/${boardId}`);
+  const response = await fetch(
+    `http://34.9.116.242:8080/api/boards/${boardId}`,
+  );
   return response.json();
 };
 
 // 게시글 수정
 export const updateBoard = async (boardId, data) => {
-  const response = await fetch(`http://localhost:8080/api/boards/${boardId}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
+  const response = await fetch(
+    `http://34.9.116.242:8080/api/boards/${boardId}`,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
     },
-    body: JSON.stringify(data),
-  });
+  );
 
   return response.json();
 };
 
 // 게시글 삭제
 export const deleteBoard = async (boardId) => {
-  await fetch(`http://localhost:8080/api/boards/${boardId}`, {
+  await fetch(`http://34.9.116.242:8080/api/boards/${boardId}`, {
     method: "DELETE",
   });
 };
 
 // 댓글 UI
 export const getComments = async (commentId) => {
-  const res = await fetch(`http://localhost:8080/api/comments/${commentId}`);
+  const res = await fetch(`http://34.9.116.242:8080/api/comments/${commentId}`);
   return res.json();
 };
 
 export const createComment = async (commentId, data) => {
-  await fetch(`http://localhost:8080/api/comments/${commentId}`, {
+  await fetch(`http://34.9.116.242:8080/api/comments/${commentId}`, {
     method: "POST",
     headers: { "Content-type": "application/json" },
     body: JSON.stringify(data),
